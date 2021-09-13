53ff9399adef5e6ab6d800eb573b1d186f13852c
Mohammad Ali Sakib <mohammad.ali.sakib.pk@gmail.com>
2021-09-13 13:15:03 +0600

    add rating amount and average rating

--- js/app.js
+++ js/app.js
@@ -21,6 +21,7 @@ const showProducts = (products) => {
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
+      <p><span> Rating: ${product.rating.rate} </span><span class='text-muted'>(${product.rating.count})</span></p>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-primary">add to cart</button>
       <button id="details-btn" class="btn btn-warning">Details</button></div>
