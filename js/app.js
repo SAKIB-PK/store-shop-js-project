da46f4e6df43ef57304dfb0a9187f0d32f4931a4
Mohammad Ali Sakib <mohammad.ali.sakib.pk@gmail.com>
2021-09-13 09:32:58 +0600

    price value add poisha

--- js/app.js
+++ js/app.js
@@ -48,12 +48,14 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+   // exact value show --value instead of  Math.round(value) 
+  document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  // exact value show --value instead of  Math.round(value) 
+  document.getElementById(id).innerText = value;
 };
 
 // update delivery charge and total Tax
